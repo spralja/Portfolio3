db9e9631d958b9ee29a46cec40065bc2e0a25ee2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-19 17:27:13 +0200

    Update Student.java
    Created and tested randomisation of a student number so that there is not student with that number

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Random;
+
+import static java.sql.DriverManager.getConnection;
 
 public  class Student {
     private static final Random rand = new Random();
@@ -15,8 +21,34 @@ public  class Student {
 
     private static int getNewRandomStudentNumber() {
         int randomStudentNumber = rand.nextInt(RAND);
-        String sql = "SELECT StudentNumber" +
-                "WHERE StudentNumber = ? FROM Students;";
+        while(true) {
+            Connection conn = null;
+            ResultSet rs = null;
+            String sql = "SELECT StudentNumber " +
+                    "FROM Students " +
+                    "WHERE StudentNumber = " + randomStudentNumber + ";";
+
+            try {
+                String url = "jdbc:sqlite:database.db";
+                conn = getConnection(url);
+                Statement statement = conn.createStatement();
+                rs = statement.executeQuery(sql);
+                if(!rs.next()) break;
+            } catch(SQLException e) {
+                e.printStackTrace();
+                return -1;
+            } finally {
+                if (conn != null) {
+                    try {
+                        conn.close();
+                    } catch(SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            randomStudentNumber = rand.nextInt(RAND);
+        }
 
         return randomStudentNumber;
     }
