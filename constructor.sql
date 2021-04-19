a153280506d3c11403cf669e80043e3f1291c9b2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-20 01:56:39 +0200

    Update constructor.sql
    Added data

--- constructor.sql
+++ constructor.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS Students;
 CREATE TABLE IF NOT EXISTS Students (
     Name TEXT NOT NULL,
     Address TEXT NOT NULL,
-    ID INTEGER NOT NULL,
-    PRIMARY KEY (ID)
+    PRIMARY KEY (Name, Address)
 );
 
 DROP TABLE IF EXISTS Courses;
@@ -17,13 +16,14 @@ CREATE TABLE IF NOT EXISTS Courses (
 
 DROP TABLE IF EXISTS Registrations;
 CREATE TABLE IF NOT EXISTS Registrations (
-    StudentID INTEGER NOT NULL,
+    StudentName TEXT NOT NULL,
+    StudentAddress TEXT NOT NULL,
     CourseName TEXT NOT NULL,
     CourseYear INTEGER NOT NULL,
     CourseSemester TEXT NOT NULL,
     Grade INTEGER,
-    FOREIGN KEY (StudentID)
-        REFERENCES Students (ID)
+    FOREIGN KEY (StudentName, StudentAddress)
+        REFERENCES Students (Name, Address)
         ON UPDATE CASCADE ON DELETE CASCADE,
 
     FOREIGN KEY (CourseName, CourseYear, CourseSemester)
@@ -40,3 +40,124 @@ VALUES ('SD', 2020, 'spring', 'Line');
 
 INSERT INTO Courses (Name, Year, Semester, Teacher)
 VALUES ('ES1', 2019, 'autumn', 'Ebbe');
+
+
+/* Aisha Linclon */
+INSERT INTO Students (Name, Address)
+VALUES ('Aisha Lincoln', '4800 Nykøbing F Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                            Grade)
+VALUES ('Aisha Lincoln', '4800 Nykøbing F Denmark', 'SD', 2019, 'autumn', 12);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Aisha Lincoln', '4800 Nykøbing F Denmark', 'ES1', 2019, 'autumn', 10);
+
+/* Anya Neilsen */
+INSERT INTO Students (Name, Address)
+VALUES ('Anya Neilsen', '4800 Nykøbing F Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Anya Neilsen', '4800 Nykøbing F Denmark', 'SD', 2020, 'spring', NULL);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Anya Neilsen', '4800 Nykøbing F Denmark', 'ES1', 2019, 'autumn', 12);
+
+/* Alfred Jensen */
+INSERT INTO Students (Name, Address)
+VALUES ('Alfred Jensen', 'Karlskrona Sweden');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Alfred Jensen', 'Karlskrona Sweden', 'SD', 2019, 'autumn', 7);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Alfred Jensen', 'Karlskrona Sweden', 'ES1', 2019, 'autumn', 10);
+
+/* Berta Bertelsen */
+INSERT INTO Students (Name, Address)
+VALUES ('Berta Bertelsen', '7190 Bilund Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Berta Bertelsen', '7190 Bilund Denmark', 'SD', 2020, 'spring', NULL);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Berta Bertelsen', '7190 Bilund Denmark', 'ES1', 2019, 'autumn', 2);
+
+/* Albert Antonsen */
+INSERT INTO Students (Name, Address)
+VALUES ('Albert Antonsen', '4180 Sorø Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Albert Antonsen', '4180 Sorø Denmark', 'SD', 2019, 'autumn', 10);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Albert Antonsen', '4180 Sorø Denmark', 'ES1', 2019, 'autumn', 7);
+
+/* Eske Eriksen */
+INSERT INTO Students (Name, Address)
+VALUES ('Eske Eriksen', '4863 Eskildstrup Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Eske Eriksen', '4863 Eskildstrup Denmark', 'SD', 2020, 'spring', NULL);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Eske Eriksen', '4863 Eskildstrup Denmark', 'ES1', 2019, 'autumn', 10);
+
+/* Olaf Olesen */
+INSERT INTO Students (Name, Address)
+VALUES ('Olaf Olesen', '5000 Odense Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Olaf Olesen', '5000 Odense Denmark', 'SD', 2019, 'autumn', 4);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Olaf Olesen', '5000 Odense Denmark', 'ES1', 2019, 'autumn', 12);
+
+/* Salma Simonsen */
+INSERT INTO Students (Name, Address)
+VALUES ('Salma Simonsen', 'Stockholm Sweden');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Salma Simonsen', 'Stockholm Sweden', 'SD', 2020, 'spring', NULL);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Salma Simonsen', 'Stockholm Sweden', 'ES1', 2019, 'autumn', 12);
+
+/* Theis Thomasen */
+INSERT INTO Students (Name, Address)
+VALUES ('Theis Thomasen', '4340 Tølløse Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Theis Thomasen', '4340 Tølløse Denmark', 'SD', 2019, 'autumn', 12);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Theis Thomasen', '4340 Tølløse Denmark', 'ES1', 2019, 'autumn', 12);
+
+/* Janet Jensen */
+INSERT INTO Students (Name, Address)
+VALUES ('Janet Jensen', '4040 Jyllinge Denmark');
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Janet Jensen', '4040 Jyllinge Denmark', 'SD', 2020, 'spring', NULL);
+
+INSERT INTO Registrations (StudentName, StudentAddress, CourseName, CourseYear, CourseSemester,
+                           Grade)
+VALUES ('Janet Jensen', '4040 Jyllinge Denmark', 'ES1', 2019, 'autumn', 7);
