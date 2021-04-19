8ba144177821c2b297f839eefd3205c8c919bca4
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-20 00:22:39 +0200

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -8,7 +8,6 @@ public class Course {
     private int year;
     private String semester;
     private String teacher;
-    private int ID;
 
     public Course(ResultSet rs) {
         try {
@@ -16,7 +15,6 @@ public class Course {
             this.year = rs.getInt(2);
             this.semester = rs.getString(3);
             this.teacher = rs.getString(4);
-            this.ID = rs.getInt(5);
         } catch(SQLException e) {
             e.printStackTrace();
         }
@@ -36,9 +34,5 @@ public class Course {
 
     public String getTeacher() {
         return this.teacher;
-    }
-
-    public int getID() {
-        return this.ID;
     }
 }
