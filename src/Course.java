953463a9de78578055e1c89eae22d452d9bdf0f2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-22 17:16:45 +0200

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,12 +1,31 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Course {
     private String name;
     private Integer year;
     private String semester;
     private String teacher;
-    public Course(String name, Integer year, String semester, String teacher) {
-        this.name = name;
-        this.year = year;
-        this.semester = semester;
-        this.teacher = teacher;
+    public Course(ResultSet rs) throws SQLException {
+        this.name = rs.getString(1);
+        this.year = rs.getInt(2);
+        this.semester = rs.getString(3);
+        this.teacher = rs.getString(4);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public String getTeacher() {
+        return teacher;
     }
 }
