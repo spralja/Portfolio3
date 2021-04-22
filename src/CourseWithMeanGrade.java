ec07eed9e4a5f18a0c2b19298e793d15ce09d4ea
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-22 22:21:50 +0200

    Update CourseWithMeanGrade.java

--- src/CourseWithMeanGrade.java
+++ src/CourseWithMeanGrade.java
@@ -2,9 +2,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CourseWithMeanGrade extends Course {
-    private Float meanGrade;
+    private String meanGrade;
     public CourseWithMeanGrade(ResultSet rs) throws SQLException {
         super(rs);
-        this.meanGrade = rs.getFloat(5);
+        this.meanGrade = rs.getString(5);
+    }
+
+    public String getMeanGrade() {
+        return meanGrade;
     }
 }
