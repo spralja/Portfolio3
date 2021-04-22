43fbe12a177f11a6cda0a0e62bc2ffa07da2dcbe
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-22 20:12:52 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class Main extends Application {
+    private static final String URL = "jdbc:sqlite:database.db";
+    private static final Integer SCENE_WIDTH = 600;
+    private static final Integer SCENE_HEIGHT = 475;
+
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Model model = new Model(URL);
+        Controller control = new Controller(model);
+        View view = new View(control);
+        control.setView(view);
+        stage.setTitle("Student Registration");
+        stage.setScene(new Scene(view.asParent(), SCENE_WIDTH, SCENE_HEIGHT));
+        stage.show();
     }
 }
