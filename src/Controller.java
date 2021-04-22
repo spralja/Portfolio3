f54feec603b842b30b68a2ad36292bb027ece5ae
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-22 20:05:53 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,27 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Controller {
     private Model model;
+    private View view;
 
     public Controller(Model model) {
         this.model = model;
+    }
+
+    public void setView(View view) {
+        this.view = view;
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public ObservableList<Student> getStudents() {
+        return FXCollections.observableArrayList(model.getStudentArrayList());
+    }
+
+    public ObservableList<Course> getCourses() {
+        return FXCollections.observableArrayList(model.getCourseArrayList());
     }
 }
