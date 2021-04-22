b92ca6fadca1585b39af4c51a0be51f8e7c6c80a
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-22 20:05:37 +0200

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -4,12 +4,19 @@ import java.sql.SQLException;
 public class Course {
     private String name;
     private Integer year;
+    private Integer yearShort;
     private String semester;
+    private Character semesterShort;
     private String teacher;
     public Course(ResultSet rs) throws SQLException {
         this.name = rs.getString(1);
         this.year = rs.getInt(2);
+        this.yearShort = year % 100;
+
         this.semester = rs.getString(3);
+        if(semester.equals("autumn")) semesterShort = 'F';
+        else semesterShort = 'E';
+
         this.teacher = rs.getString(4);
     }
 
@@ -27,5 +34,10 @@ public class Course {
 
     public String getTeacher() {
         return teacher;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s-%c%d", name, semesterShort, yearShort);
     }
 }
